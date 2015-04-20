b85395f98428996828faa7819acab2b84cfd7478
Rizmari Versfeld <rizziepit@gmail.com>
2015-04-20 10:37:54 +0200

    add methods to check for and get previous/next page args

--- unicore/comments/client/__init__.py
+++ unicore/comments/client/__init__.py
@@ -1,6 +1,7 @@
 from unicore.comments.client.base import CommentServiceException
 from unicore.comments.client.commentclient import (
-    CommentClient, Comment, CommentPage, UserBanned, CommentStreamNotOpen)
+    CommentClient, Comment, CommentPage, UserBanned, CommentStreamNotOpen,
+    LazyCommentPage)
 
 
 __all__ = [
@@ -9,5 +10,6 @@ __all__ = [
     'CommentClient',
     'CommentPage',
     'CommentStreamNotOpen',
+    'LazyCommentPage',
     'UserBanned'
 ]
